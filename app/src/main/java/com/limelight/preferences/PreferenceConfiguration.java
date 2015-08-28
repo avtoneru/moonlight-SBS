e791e8f1b5d0ac11ac9c337947bf8c66b1f1a7e7
Cameron Gutman <aicommander@gmail.com>
2015-08-27 21:02:36 -0700

    Use a builder for PreferenceConfiguration and add an option to suppress the latency toast

--- app/src/main/java/com/limelight/preferences/PreferenceConfiguration.java
+++ app/src/main/java/com/limelight/preferences/PreferenceConfiguration.java
@@ -19,6 +19,7 @@ public class PreferenceConfiguration {
     private static final String LIST_MODE_PREF_STRING = "checkbox_list_mode";
     private static final String SMALL_ICONS_PREF_STRING = "checkbox_small_icon_mode";
     private static final String MULTI_CONTROLLER_PREF_STRING = "checkbox_multi_controller";
+    private static final String NO_LATENCY_TOAST_PREF_STRING = "checkbox_no_latency_toast";
 
     private static final int BITRATE_DEFAULT_720_30 = 5;
     private static final int BITRATE_DEFAULT_720_60 = 10;
@@ -36,6 +37,7 @@ public class PreferenceConfiguration {
     public static final String DEFAULT_LANGUAGE = "default";
     private static final boolean DEFAULT_LIST_MODE = false;
     private static final boolean DEFAULT_MULTI_CONTROLLER = true;
+    private static final boolean DEFAULT_NO_LATENCY_TOAST = false;
 
     public static final int FORCE_HARDWARE_DECODER = -1;
     public static final int AUTOSELECT_DECODER = 0;
@@ -48,6 +50,7 @@ public class PreferenceConfiguration {
     public boolean stretchVideo, enableSops, playHostAudio, disableWarnings;
     public String language;
     public boolean listMode, smallIconMode, multiController;
+    public boolean suppressLatencyToast;
 
     public static int getDefaultBitrate(String resFpsString) {
         if (resFpsString.equals("720p30")) {
@@ -131,52 +134,125 @@ public class PreferenceConfiguration {
 
     public static PreferenceConfiguration readPreferences(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        PreferenceConfiguration config = new PreferenceConfiguration();
+        PreferenceConfiguration.Builder builder = new PreferenceConfiguration.Builder();
 
-        config.bitrate = prefs.getInt(BITRATE_PREF_STRING, getDefaultBitrate(context));
+        builder.setBitrate(prefs.getInt(BITRATE_PREF_STRING, getDefaultBitrate(context)));
         String str = prefs.getString(RES_FPS_PREF_STRING, DEFAULT_RES_FPS);
         if (str.equals("720p30")) {
-            config.width = 1280;
-            config.height = 720;
-            config.fps = 30;
+            builder.setResolution(1280, 720);
+            builder.setFps(30);
         }
         else if (str.equals("720p60")) {
-            config.width = 1280;
-            config.height = 720;
-            config.fps = 60;
+            builder.setResolution(1280, 720);
+            builder.setFps(60);
         }
         else if (str.equals("1080p30")) {
-            config.width = 1920;
-            config.height = 1080;
-            config.fps = 30;
+            builder.setResolution(1920, 1080);
+            builder.setFps(30);
         }
         else if (str.equals("1080p60")) {
-            config.width = 1920;
-            config.height = 1080;
-            config.fps = 60;
+            builder.setResolution(1920, 1080);
+            builder.setFps(60);
         }
-        else {
-            // Should never get here
+
+        // Standard streaming preferences
+        builder.setDisableWarnings(prefs.getBoolean(DISABLE_TOASTS_PREF_STRING, DEFAULT_DISABLE_TOASTS));
+        builder.setEnableSops(prefs.getBoolean(SOPS_PREF_STRING, DEFAULT_SOPS));
+        builder.setStretchVideo(prefs.getBoolean(STRETCH_PREF_STRING, DEFAULT_STRETCH));
+        builder.setPlayHostAudio(prefs.getBoolean(HOST_AUDIO_PREF_STRING, DEFAULT_HOST_AUDIO));
+        builder.setMultiController(prefs.getBoolean(MULTI_CONTROLLER_PREF_STRING, DEFAULT_MULTI_CONTROLLER));
+        builder.setSuppressLatencyToast(prefs.getBoolean(NO_LATENCY_TOAST_PREF_STRING, DEFAULT_NO_LATENCY_TOAST));
+        builder.setDeadzonePercentage(prefs.getInt(DEADZONE_PREF_STRING, DEFAULT_DEADZONE));
+
+        // Internal preferences
+        PreferenceConfiguration config = builder.build();
+        config.decoder = getDecoderValue(context);
+        config.language = prefs.getString(LANGUAGE_PREF_STRING, DEFAULT_LANGUAGE);
+        config.smallIconMode = prefs.getBoolean(SMALL_ICONS_PREF_STRING, getDefaultSmallMode(context));
+        config.listMode = prefs.getBoolean(LIST_MODE_PREF_STRING, DEFAULT_LIST_MODE);
+
+        return config;
+    }
+
+    public static class Builder {
+        private PreferenceConfiguration config;
+
+        public Builder() {
+            this.config = new PreferenceConfiguration();
+
+            // Set defaults
             config.width = 1280;
             config.height = 720;
             config.fps = 60;
+            config.bitrate = DEFAULT_BITRATE;
+            config.disableWarnings = DEFAULT_DISABLE_TOASTS;
+            config.enableSops = DEFAULT_SOPS;
+            config.stretchVideo = DEFAULT_STRETCH;
+            config.playHostAudio = DEFAULT_HOST_AUDIO;
+            config.listMode = DEFAULT_LIST_MODE;
+            config.multiController = DEFAULT_MULTI_CONTROLLER;
+            config.deadzonePercentage = DEFAULT_DEADZONE;
+            config.suppressLatencyToast = DEFAULT_NO_LATENCY_TOAST;
+
+            // Internal preferences
+            config.decoder = AUTOSELECT_DECODER;
+            config.language = DEFAULT_LANGUAGE;
+            config.smallIconMode = false;
         }
 
-        config.decoder = getDecoderValue(context);
+        public Builder setResolution(int width, int height) {
+            config.width = width;
+            config.height = height;
+            return this;
+        }
 
-        config.deadzonePercentage = prefs.getInt(DEADZONE_PREF_STRING, DEFAULT_DEADZONE);
+        public Builder setFps(int fps) {
+            config.fps = fps;
+            return this;
+        }
 
-        config.language = prefs.getString(LANGUAGE_PREF_STRING, DEFAULT_LANGUAGE);
+        public Builder setDisableWarnings(boolean disabled) {
+            config.disableWarnings = disabled;
+            return this;
+        }
 
-        // Checkbox preferences
-        config.disableWarnings = prefs.getBoolean(DISABLE_TOASTS_PREF_STRING, DEFAULT_DISABLE_TOASTS);
-        config.enableSops = prefs.getBoolean(SOPS_PREF_STRING, DEFAULT_SOPS);
-        config.stretchVideo = prefs.getBoolean(STRETCH_PREF_STRING, DEFAULT_STRETCH);
-        config.playHostAudio = prefs.getBoolean(HOST_AUDIO_PREF_STRING, DEFAULT_HOST_AUDIO);
-        config.listMode = prefs.getBoolean(LIST_MODE_PREF_STRING, DEFAULT_LIST_MODE);
-        config.smallIconMode = prefs.getBoolean(SMALL_ICONS_PREF_STRING, getDefaultSmallMode(context));
-        config.multiController = prefs.getBoolean(MULTI_CONTROLLER_PREF_STRING, DEFAULT_MULTI_CONTROLLER);
+        public Builder setBitrate(int bitrate) {
+            config.bitrate = bitrate;
+            return this;
+        }
 
-        return config;
+        public Builder setEnableSops(boolean sops) {
+            config.enableSops = sops;
+            return this;
+        }
+
+        public Builder setStretchVideo(boolean stretch) {
+            config.stretchVideo = stretch;
+            return this;
+        }
+
+        public Builder setPlayHostAudio(boolean hostAudio) {
+            config.playHostAudio = hostAudio;
+            return this;
+        }
+
+        public Builder setMultiController(boolean enabled) {
+            config.multiController = enabled;
+            return this;
+        }
+
+        public Builder setDeadzonePercentage(int percentage) {
+            config.deadzonePercentage = percentage;
+            return this;
+        }
+
+        public Builder setSuppressLatencyToast(boolean suppress) {
+            config.suppressLatencyToast = suppress;
+            return this;
+        }
+
+        public PreferenceConfiguration build() {
+            return config;
+        }
     }
 }
